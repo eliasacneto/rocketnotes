28422dcd46e29e9bbc1a5ba78235ecd5af45f0cc
Elias Neto <netoeac.dev@gmail.com>
2024-05-14 20:49:48 -0300

    update

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+                <h1>Rocketnotes</h1>
+            </Brand>
+
+            <Menu>
+
+            </Menu>
+
+            <Search>
+
+
+            </Search>
+
+            <Content>
+
+            </Content>
+        </Container>
+
+    );
+}
